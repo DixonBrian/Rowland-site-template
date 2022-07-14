dc13f284645e024fb6dc833a8d15028e00403fc0
Brian Dixon <brian@rowlandcreative.com>
2022-07-14 13:36:27 -0400

    adding Pantheon Security mods

--- wp-content/themes/rowland-clone/functions.php
+++ wp-content/themes/rowland-clone/functions.php
@@ -262,3 +262,32 @@ function wpb_custom_new_menu() {
   );
 }
 add_action( 'init', 'wpb_custom_new_menu' );
+
+// Disable WP Users REST API for non-authenticated users (allows anyone to see username list at /wp-json/wp/v2/users)
+add_filter( 'rest_authentication_errors', function( $result ) {
+	if ( true === $result || is_wp_error( $result ) ) {
+		return $result;
+	}
+
+	if ( ! is_user_logged_in() ) {
+		return new WP_Error(
+			'rest_not_logged_in',
+			__( 'You are not currently logged in.' ),
+			array( 'status' => 401 )
+		);
+	}
+
+	return $result;
+});
+
+// Adding custiom headers
+function additional_securityheaders( $headers ) {
+    $headers['Referrer-Policy']             = 'no-referrer';
+    $headers['X-Content-Type-Options']      = 'nosniff';
+    $headers['X-XSS-Protection']            = '1; mode=block';
+    $headers['Permissions-Policy']          = 'fullscreen=(self "https://***Update with site url****"), geolocation=*, camera=()';
+    $headers['X-Frame-Options']             = "SAMEORIGIN";
+    $headers ['Strict-Transport-Security']  = "max-age=31536000";
+  return $headers;
+}
+add_filter( 'wp_headers', 'additional_securityheaders' );
